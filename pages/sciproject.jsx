227f0a6c845867c0428d23f04174bc5c21318e18
NitaJongkraijak <nitajkj4250@gmail.com>
2023-08-20 22:52:41 +0700

    .

--- pages/sciproject.jsx
+++ pages/sciproject.jsx
@@ -48,7 +48,13 @@ export default function SciProject(){
   ];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [showFullDescription, setShowFullDescription] = useState(false);
+  const [selectedDateIndex, setSelectedDateIndex] = useState(0);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
+  const openImageSlide = (dateIndex, imageIndex) => {
+    setSelectedDateIndex(dateIndex);
+    setSelectedImageIndex(imageIndex);
+  };
 
   const showNextImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -117,7 +123,7 @@ export default function SciProject(){
           content='https://openhouse.mwit.ac.th/img/ogimage.png'
         />   
     </Head>
-    <div className='flex flex-col w-full -z-10'>
+    <div className='flex flex-col w-full '>
       <div className='w-full bg-cover bg-[url(/img/2023/bg_starlight.webp)] bg-repeat sm:bg-fixed text-black items-center mx-auto justify-self-center w-full max-w-6xl px-6 pt-10 pb-4 '>
         <span className='flex justify-center font-CS font-bold text-3xl xmd:text-4xl lg:text-5xl pt-16 text-bmw'>
               MWIT Science Project
@@ -219,7 +225,23 @@ export default function SciProject(){
           </div>
                   */}
 
-        <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-lg">
+        
+        </div>
+
+        <div className="w-full max-w-7xl grid grid grid-cols-2 justify-items-center mx-auto px-6"> 
+        <div className="max-w-[640px] max-h-[480px] min-w-max w-[16rem] md:w-[32rem] h-screen m-auto py-8 px-4 relative group ">
+            <div
+              className="w-full h-full rounded-2xl bg-center bg-cover duration-100"
+              style={{ backgroundImage: `url(${images[currentImageIndex].src})` }}
+              ></div>
+             <div className='group-hover:block absolute top-[50%] t-ranslate-x-100 translate-y-[-50%] left-5 text-2xl rounded-full p-3 bg-black/40 text-white cursor-pointer '>
+                  <FontAwesomeIcon icon={faChevronLeft} onClick={showPrevImage} size='lg' />
+                </div>
+              <div className='group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-3 bg-black/40 text-white cursor-pointer'>
+                  <FontAwesomeIcon icon={faChevronRight} onClick={showNextImage} size='lg' />
+            </div>
+        </div>
+        <div className="flex-1 w-full max-w-lg p-8 bg-white rounded-lg shadow-lg">
         <div className="font-CS font-bold text-bmw text-xl md:text-3xl lg:text-4xl text-2xl font-semibold ">กำหนดการ</div>
         {scheduleData.map((dayData, index) => (
           <div key={index} className="mb-6">
@@ -238,7 +260,7 @@ export default function SciProject(){
           </div>
           ))}
           </div>
-        </div>
+          </div>
       </div>
     </div>
   </>
