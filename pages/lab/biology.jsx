faa9b6e7ca002dba9030deadc5e7035d7bd4cbd3
chadinomoto <134485577+chadinomoto@users.noreply.github.com>
2023-08-21 19:00:29 +0700

    ใใ

--- pages/lab/biology.jsx
+++ pages/lab/biology.jsx
@@ -118,7 +118,8 @@ import { faFlaskVial,
               </Link>
               </div>
               <div className='flex flex-col text-center text-black items-center mx-auto justify-self-center w-full max-w-6xl px-6 pt-10 pb-4'>
-              <div className='flex relative flex-col gap-2 items-center shadow-lg py-3 px-4 bg-pink-200/40 backdrop-blur-md rounded-xl'>
+            <div className='flex relative flex-col gap-2 items-center shadow-lg py-3 px-4 bg-pink-200/40 backdrop-blur-md rounded-xl'>
+              <div className='flex flex-col text-center text-black items-center mx-auto justify-self-center w-full max-w-6xl px-6 pt-10 pb-4'>
               <span className='text-lg md:text-xl lg:text-2xl font-CS font-semibold text-center'>
                 กลุ่มสาระสุขศึกษาและพลศึกษา
               </span>
@@ -153,7 +154,7 @@ import { faFlaskVial,
                   <span>ร่วมตอบคำถามลุ้นรางวัล</span>
                 </a>
               </Link>
-            
+            </div>
             </div>
             {peData.map((l, li) => (
               <div
