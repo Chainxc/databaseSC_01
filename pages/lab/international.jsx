cceffd6660b955ef6d2d426881c569b14744416b
chadinomoto <134485577+chadinomoto@users.noreply.github.com>
2023-08-21 20:45:31 +0700

    ด ้เ

--- pages/lab/international.jsx
+++ pages/lab/international.jsx
@@ -66,10 +66,12 @@ import { faFlaskVial,
                 </div>
               <div className='flex relative flex-col gap-2 items-center shadow-lg py-3 px-4 bg-pink-200/40 backdrop-blur-md rounded-xl'>
                 <div className='flex flex-col text-center text-black items-center mx-auto justify-self-center w-full max-w-6xl px-6 pt-10 pb-4'>
+                <span className='text-lg md:text-xl lg:text-2xl font-CS font-semibold text-center mt-2'>
+                จกรรม “discovery of the spooky tales”
+                </span>
                 <span className='text-lg md:text-xl lg:text-2xl font-CS font-semibold text-center'>
                 ห้องที่ 2 - ผีอังกฤษ (ผีแฟรงเกนสไตน์)
                 </span>
-                <div className='grid grid-flow-row md:grid-flow-col gap-4 justify-items-center'>
                   <div className='flex flex-col justify-center gap-2'>
                     <span className='font-IBMPlexLoop text-sm md:text-base font-semibold'>
                     •วิธีเล่นเกม
@@ -78,7 +80,6 @@ import { faFlaskVial,
                     ไกด์จะเดินตามมาเเล้วบรีฟว่าต้องทำเพื่อคืนชีพแฟงค์กินสไลล์ โดยเริ่มเเรกต้องไปเล่มเกมสั้นๆ เช่นเกม tongue twister กับคนเเรกเพื่อคืนชีพเเขน เเขนผีเเฟงค์ก็จะกระตุกเเล้วชี้ไปยังคนที่สองเพื่อไปเล่นเกมเเล้วฟื้นคืนชีพหัว ผีเเฟงค์ก็จะพูดได้เเล้ว โดยบอกผู้เล่นให้ไปเเปล riddle เเล้วหาคำตอบไฟฟื้นชีพขา เมื่อฟื้นครบทุกส่วน ผีเเฟงค์จะตื่นขึ้นเเล้วช่วยให้ไปห้องต่อไปได้
                     </span >
                   </div>
-                </div>
               </div>
 
               </div>
